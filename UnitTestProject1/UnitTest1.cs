0548776c041d8ad917684fe7ec49112e54702575
Hielko2 <44575202+Hielko2@users.noreply.github.com>
2018-12-06 14:42:59 +0100

    Init

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MUT.Reply;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestReplaceBlockPeriod()
+        {
+         
+            if (true)
+            {
+                BlockPeriod p = new BlockPeriod { Time = "11:30-12:30" };
+                Assert.IsTrue(p.IsMatch(new DateTime(2000, 8, 1, 11, 31, 0)), "2001, 8, 1, 11, 31");
+                Assert.IsFalse(p.IsMatch(new DateTime(2000, 8, 1, 11, 29, 0)), "2002, 8, 1, 11, 29");
+            }
+
+            {
+                BlockPeriod p = new BlockPeriod { Time = "23:30-02:30" };
+                Assert.IsTrue(p.IsMatch(new DateTime(2000, 8, 1, 23, 31, 0)), p.ToString() + " 2003, 8, 1, 11, 31");
+                Assert.IsTrue(p.IsMatch(new DateTime(2006, 8, 1, 1, 1, 0)), p.ToString() + "  2004, 8, 1, 1, 1");
+                Assert.IsFalse(p.IsMatch(new DateTime(2000, 9, 1, 2, 31, 0)), p.ToString() + " 2005, 9, 1, 2, 31");
+            }
+
+            {
+                BlockPeriod p = new BlockPeriod { Days = "Saturday" };
+                Assert.IsTrue(p.IsMatch(new DateTime(2018, 9, 1, 23, 31, 0)), p.ToString() + " is Saterday");
+                p.Time = "12:00-13:00";
+                Assert.IsFalse(p.IsMatch(new DateTime(2018, 9, 2, 12, 0, 0)), p.ToString() + " is Saterday not in timeperiode");
+            }
+
+            {
+                BlockPeriod p = new BlockPeriod { Time = "01:00-11:00", Days = "Monday,Tuesday,Wednesday,Thursday,Friday" };
+                Assert.IsTrue(p.IsMatch(new DateTime(2018, 8, 30, 10, 59, 0)), p.ToString() + " zzzzzzzzzzz");
+            }
+
+    
+            {
+                BlockPeriod p = new BlockPeriod { Time = "23:30-06:00", Days = "Monday" };
+                Assert.IsTrue(p.IsMatch(new DateTime(2018, 8, 27, 23, 50, 0)), p.ToString() + " Block ");
+                Assert.IsTrue(p.IsMatch(new DateTime(2018, 8, 27, 0, 50, 0)), p.ToString() + " Block ");
+            }
+
+        }
+
+
+        [TestMethod]
+        public void TestReplaceRandomTokens()
+        {
+
+            {
+                string result = Utils.StringUtils.ReplaceRandomTokens("{1}");
+                //Assert.IsTrue(("1", result);
+            }
+
+            {
+                string result = Utils.StringUtils.ReplaceRandomTokens("{}");
+                Assert.AreEqual("", result);
+            }
+            {
+                string result = Utils.StringUtils.ReplaceRandomTokens("{1,2}");
+                if (!(result.Equals("1") || result.Equals("2")))
+                {
+                    Assert.Fail("must be 1 or 2");
+                }
+            }
+
+            {
+                string result = Utils.StringUtils.ReplaceRandomTokens("{1,}");
+                if (!(result.Equals("1") || result.Equals("")))
+                {
+                    Assert.Fail("must be 1 or empty");
+                }
+            }
+
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MUT.Reply;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string[] c = { "1", "2","a" };
+            RuleGroup r = new RuleGroup { Contains = c };
+            Assert.IsTrue(r.IsMatch("1"), "Contains 1");
+            Assert.IsTrue(r.IsMatch("2"), "Contains 2");
+            Assert.IsTrue(r.IsMatch("a"), "Contains a");
+            Assert.IsTrue(r.IsMatch("A"), "Contains A");
+            Assert.IsFalse(r.IsMatch("3"), "not Contains 3");
+
+
+            string[] startwith = { "this", "is", "a", "centence" };
+            r = new RuleGroup { Startswith = startwith };
+            Assert.IsTrue(r.IsMatch("this is a centence"), "Startswith 'this'");
+            Assert.IsTrue(r.IsMatch("is this a centence"), "Startswith 'is' ");
+            Assert.IsFalse(r.IsMatch("none"), "Startswith not with 'none' ");
+
+        }
+    }
+}
